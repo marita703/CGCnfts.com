2deefe23ed8a567e18c55e4ef8c8ba0c08eae46a
Maria del Mar <chocoinformatics2016@gmail.com>
2023-05-13 13:43:03 +0200

    added more components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
+import Footer from "@/src/Components/Footer";
+import Navbar from "@/src/Components/Navbar";
+import Section1 from "@/src/container/Home/Section1";
+import Section2 from "@/src/container/Home/Section2";
 import React from "react";
 
 function Home() {
-  return <div>Home hola maria como vas</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Section1 />
+      <Section2 />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
