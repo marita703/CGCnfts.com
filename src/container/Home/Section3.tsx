d573c04ef76037eb3079ecc09a8dc2bbf8906fad
Maria del Mar <chocoinformatics2016@gmail.com>
2023-05-17 19:54:10 +0200

    put some text and need to figure it out how to manage it to look beautiful

--- src/container/Home/Section3.tsx
+++ src/container/Home/Section3.tsx
@@ -1,35 +1,90 @@
-import { Box, BoxProps, Container, Stack, StackProps } from "@mui/material";
+import {
+  Box,
+  BoxProps,
+  Container,
+  Grid,
+  Stack,
+  StackProps,
+  Typography,
+} from "@mui/material";
 import Item from "../../Components/Card/Item";
 import React, { useEffect, useRef } from "react";
 
 function Section3() {
   return (
-    <Container sx={{ height: "100vh", mb: "2em" }}>
-      <Stack
-        sx={{ height: "90%", width: "90%", m: "3em" }}
-        direction="row"
-        alignContent="center"
-      >
+    <Stack
+      sx={{ height: "100vh", width: "90vw" }}
+      direction="row"
+      alignContent="center"
+      justifyItems="center"
+    >
+      <Box sx={{ height: "70% ", width: "100%", mb: "2em" }}>
         <Stack
-          display="grid"
-          sx={{ height: "100%", width: "100%", m: "3em" }}
-          border="3px solid white"
-          position="relative"
+          sx={{
+            height: "100%",
+            width: "100%",
+            m: "3em",
+            background:
+              "linear-gradient(transparent, #5d0b55, #8e076c, transparent)",
+            backdropFilter: "blur(10px)",
+          }}
+          // remember why you put this
+          overflow="hidden"
         >
-          <Item
-            sx={{
-              top: "0",
-              left: "0",
-              backgroundColor: "hotpink",
-              heigth: "60px",
-              width: "60px",
-            }}
-          >
-            holi
-          </Item>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Typography
+                variant="h3"
+                sx={{ p: "3rem" }}
+                alignContent="center"
+                justifyContent="center"
+                fontWeight="bold"
+              >
+                CGC: Your portal to web3
+              </Typography>
+            </Grid>
+
+            <Grid item xs={8} alignContent="center" justifyContent="center">
+              <Typography variant="body1" sx={{ p: "3rem" }}>
+                CGC is the community that grants you access. The new version of
+                the internet is for everyone. We offer a no-bounds, community
+                for people who want to enter web3 but lack the means to get
+                started.
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Stack direction="row" justifyContent="space-between">
+            <Typography
+              sx={{
+                p: "3rem",
+              }}
+              variant="h2"
+            >
+              Why are we doing this?
+            </Typography>
+          </Stack>
+
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Typography variant="h4" sx={{ p: "3rem" }} fontWeight="bold">
+                The future of entrepreneurship is web3
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h4" sx={{ p: "3rem" }} fontWeight="bold">
+                The internet changed our lives. Web3 will do too.
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h4" sx={{ p: "3rem" }} fontWeight="bold">
+                We need powerful new voices to drive the web3.
+              </Typography>
+            </Grid>
+          </Grid>
         </Stack>
-      </Stack>
-    </Container>
+      </Box>
+    </Stack>
   );
 }
 
