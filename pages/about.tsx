cc9460ee3c15cf2fe6e918c55fbe04a01b150d48
Maria del Mar <chocoinformatics2016@gmail.com>
2023-05-18 14:14:42 +0200

    before creating the about section

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Layout from "@/src/Components/Layout/Layout";
-import { Box } from "@mui/material";
+import { Box, Grid, Stack } from "@mui/material";
+import Lines from "../public/BackgroundImages/lines.png";
 
 function about() {
   return (
     <Layout>
-      <Box></Box>
+      <Stack
+        sx={{
+          direction: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Grid container sx={{}}></Grid>
+      </Stack>
     </Layout>
   );
 }
