f6c19ef4faa13e31073dd60b2b43477b106a6e3c
Maria del Mar <chocoinformatics2016@gmail.com>
2023-05-18 12:10:55 +0200

    finished layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,18 @@
-import Footer from "@/src/Components/Footer";
-import Navbar from "@/src/Components/Navbar";
 import Section1 from "@/src/container/Home/Section1";
 import Section2 from "@/src/container/Home/Section2";
 import Section3 from "@/src/container/Home/Section3";
-import { Box } from "@mui/material";
+import Layout from "@/src/Components/Layout/Layout";
 import React from "react";
 
 function Home() {
   return (
-    <Box>
-      <Navbar />
-
-      {/* the property overflow = hidden ,is for the component not to start creating so much space on the x scroll bar */}
-      <Box sx={{ position: "relative", overflow: "hidden" }}>
+    <>
+      <Layout>
         <Section1 />
         <Section2 />
         <Section3 />
-      </Box>
-
-      <Footer />
-    </Box>
+      </Layout>
+    </>
   );
 }
 
