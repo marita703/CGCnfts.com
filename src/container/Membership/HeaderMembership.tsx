cd041f901c74e80171b8633fe526ee9da4d54b20
Maria del Mar <chocoinformatics2016@gmail.com>
2023-05-23 09:34:48 +0200

    put the first items from the header

--- src/container/Membership/HeaderMembership.tsx
+++ src/container/Membership/HeaderMembership.tsx
@@ -1,25 +1,64 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 import React from "react";
 
 function HeaderMembership() {
   return (
-    <Box>
+    <Stack
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ p: "4rem" }}
+    >
       <Stack direction="row">
-        <h1>We’re glad you made it.</h1>
-        <span></span>
+        <h1 style={{ fontSize: "3rem" }}>We’re glad you made it.</h1>
+        <span
+          style={{
+            width: "0.4em",
+            height: "6em",
+            background:
+              "linear-gradient(0deg, #c71c81 0%, #1c22c7 50%, #7e1679 100%)",
+            filter: "drop-shadow(0 0 0.5em #c71c81)",
+            margin: "0 2em",
+          }}
+        ></span>
         <p>
           JOIN THE GENESIS MEMBERSHIP: Limited access for the first ever batch
           of Crypto Girls Club membership collection.
         </p>
       </Stack>
-      <div>
-        <a href="#JoinUs">Join us</a>
+      <Stack direction="row">
+        <Button
+          href="#JoinUs"
+          sx={{
+            padding: "0.7rem 2rem",
+            borderRadius: "0.3em",
+            textTransform: "uppercase",
+            transition: "0.3s",
+            background: "linear-gradient(90deg, #c71c81, #0798d6)",
+            color: "black",
+            border: "none",
+            "&hover": {
+              background: "#e277ae",
+            },
+          }}
+        >
+          Join us
+        </Button>
 
-        <a href="https://www.cryptogirlsclub.org/about" target="_blank">
+        <Button
+          href="/about"
+          target="_blank"
+          sx={{
+            padding: "0.7rem 2rem",
+            borderRadius: "0.3em",
+            textTransform: "uppercase",
+            transition: "0.3s",
+          }}
+        >
           Learn More
-        </a>
-      </div>
-    </Box>
+        </Button>
+      </Stack>
+    </Stack>
   );
 }
 
