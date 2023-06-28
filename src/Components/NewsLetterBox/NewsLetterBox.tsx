0b090f7ca45c10cc2f4b41489451b0a8c08956c8
Maria del Mar <chocoinformatics2016@gmail.com>
2023-06-28 17:14:26 +0200

    put title to the newsletter box

--- src/Components/NewsLetterBox/NewsLetterBox.tsx
+++ src/Components/NewsLetterBox/NewsLetterBox.tsx
@@ -6,6 +6,7 @@ function NewsLetterBox() {
   return (
     <Box id="newsletter" sx={{ m: "3rem" }}>
       <Stack direction="column" alignItems="center" spacing={4}>
+        <h2>Join the club, stay in touch</h2>
         <Typography>
           Sign up to get all the latest on our events across Europe and stay on
           top of the CGC universe.
