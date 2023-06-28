f59d6e7d53e55af7065c2c95c4c1551e5a8bfa0d
Maria del Mar <chocoinformatics2016@gmail.com>
2023-06-28 16:49:52 +0200

    made the buttons size smaller

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -25,19 +25,34 @@ function Footer() {
         >
           {/* right side */}
           <Stack direction={{ xs: "column", sm: "row" }} spacing={3}>
-            <MenuButton href="https://discord.com/invite/YpC7qw22Pb">
+            <MenuButton
+              sx={{ fontSize: "12px" }}
+              href="https://discord.com/invite/YpC7qw22Pb"
+            >
               Discord
             </MenuButton>
-            <MenuButton href="https://twitter.com/CryptoGirlsClub">
+            <MenuButton
+              sx={{ fontSize: "12px" }}
+              href="https://twitter.com/CryptoGirlsClub"
+            >
               Twitter
             </MenuButton>
-            <MenuButton href="https://www.instagram.com/cryptogirlsclub.berlin/">
+            <MenuButton
+              sx={{ fontSize: "12px" }}
+              href="https://www.instagram.com/cryptogirlsclub.berlin/"
+            >
               Instagram
             </MenuButton>
-            <MenuButton href="https://t.me/joinchat/n6z0tNyZC3IxYmU0">
+            <MenuButton
+              sx={{ fontSize: "12px" }}
+              href="https://t.me/joinchat/n6z0tNyZC3IxYmU0"
+            >
               Telegram
             </MenuButton>
-            <MenuButton href="https://www.linkedin.com/authwall?trk=gf&trkInfo=AQHlUmcO7z0NNwAAAYglFjOApldnjCXAOWb9C9cULH7X50YGKIYr4DDv7ZnETZpNOZyzFZZ_Hr_H5P2akx-TzO95JS1hnCYe5SSHcdFAGIz5nsJ33i0SFC_MdVcXlmV46-3zSJA=&original_referer=https://www.linkedin.com/company/crypto-girls-club/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fcrypto-girls-club%2F%3Foriginal_referer%3Dhttps%253A%252F%252Fcgcnfts.com%252F">
+            <MenuButton
+              sx={{ fontSize: "12px" }}
+              href="https://www.linkedin.com/authwall?trk=gf&trkInfo=AQHlUmcO7z0NNwAAAYglFjOApldnjCXAOWb9C9cULH7X50YGKIYr4DDv7ZnETZpNOZyzFZZ_Hr_H5P2akx-TzO95JS1hnCYe5SSHcdFAGIz5nsJ33i0SFC_MdVcXlmV46-3zSJA=&original_referer=https://www.linkedin.com/company/crypto-girls-club/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fcrypto-girls-club%2F%3Foriginal_referer%3Dhttps%253A%252F%252Fcgcnfts.com%252F"
+            >
               Linkedin
             </MenuButton>
           </Stack>
