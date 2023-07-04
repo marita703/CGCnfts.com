f95478638ec37fa2e4a954d84e00dffa734f4e09
Maria del Mar <chocoinformatics2016@gmail.com>
2023-07-04 16:54:40 +0200

    before ruining everything

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,6 +7,7 @@ import OurRoots from "@/src/container/About/OurRoots";
 import Header from "@/src/container/About/Header";
 import NewsLetterBox from "@/src/Components/NewsLetterBox/NewsLetterBox";
 import CarrouselPart from "@/src/container/About/CarrouselPart";
+import VideoPartLisk from "@/src/container/About/videoPartLisk";
 
 function about() {
   return (
@@ -14,6 +15,7 @@ function about() {
       <Stack overflow="hidden">
         <Header />
         <OurRoots />
+
         <CarrouselPart />
         <NewsLetterBox />
       </Stack>
---
+++ src/container/Home/videoPartVictoria.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function videoPartVictoria() {
+  return <div>videoPartVictoria</div>;
+}
+
+export default videoPartVictoria;
