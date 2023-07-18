bc7576d7378b199810f23d1b41b7b4dfc6ed0f69
Maria del Mar <chocoinformatics2016@gmail.com>
2023-07-18 09:57:33 +0200

    changed the v

--- src/container/Home/Section3.tsx
+++ src/container/Home/Section3.tsx
@@ -10,7 +10,7 @@ import {
 } from "@mui/material";
 
 import React, { useEffect, useRef } from "react";
-import VideoPartVictoria from "@/src/container/Home/VideoPartVictoria";
+import VideoPartVictoria from "@/src/container/Home/videoPartVictoria";
 
 function Section3() {
   const theme = useTheme();
