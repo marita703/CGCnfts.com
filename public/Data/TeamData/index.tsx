c459e06c6fd8d508ce50df98ec98f07412b042cb
Maria del Mar <chocoinformatics2016@gmail.com>
2023-07-18 11:07:52 +0200

    added team pictures and data

---
+++ public/Data/TeamData/index.tsx
@@ -0,0 +1,50 @@
+interface teamData {
+  name: string;
+  url: string;
+  rol: string;
+}
+
+const TeamData: teamData[] = [
+  {
+    name: "Victoria Hoffman",
+    url: "/Images/TeamImages/1.png",
+    rol: "Co-founder",
+  },
+  {
+    name: "Zoe Faircloth",
+    url: "/Images/TeamImages/2.png",
+    rol: "Co-founder",
+  },
+  {
+    name: "Rebekka Revel",
+    url: "/Images/TeamImages/3.png",
+    rol: "Community Manager",
+  },
+  {
+    name: "Mar",
+    url: "/Images/TeamImages/4.png",
+    rol: "Lead Developer",
+  },
+  {
+    name: "Marta Kalina",
+    url: "/Images/TeamImages/5.png",
+    rol: "Community Manager",
+  },
+  {
+    name: "Jean Feier",
+    url: "/Images/TeamImages/6.png",
+    rol: "Managing Director",
+  },
+  {
+    name: "Elis Yurtsever",
+    url: "/Images/TeamImages/7.png",
+    rol: "Art Director",
+  },
+  {
+    name: "Julianne Verissimo",
+    url: "/Images/TeamImages/8.png",
+    rol: "Community Manager",
+  },
+];
+
+export default TeamData;
