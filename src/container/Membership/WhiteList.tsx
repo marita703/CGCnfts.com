3b16a8f7cde5d7daa77672e3cd8b5b8c455e3d66
Maria del Mar <chocoinformatics2016@gmail.com>
2023-08-11 16:24:09 +0200

    Added Zoes image

--- src/container/Membership/WhiteList.tsx
+++ src/container/Membership/WhiteList.tsx
@@ -3,7 +3,7 @@ import WhiteListForm from "@/src/Components/WhiteListForm/WhiteListForm";
 import { Box, Grid, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
-import DiscordLogo from "../../../public/Images/DiscordLogo/concours-discord-cartes-voeux-fortnite-france-6.png";
+import DiscordLogo from "../../../public/Images/DiscordLogo/photo_5381914521393813540_y.jpg";
 
 function WhiteList() {
   return (
@@ -17,51 +17,15 @@ function WhiteList() {
       <div id="joinWhitelist">
         <h2>Join us</h2>
       </div>
-
-      <Grid container spacing={4}>
-        <Grid item xs={12} sm={12} md={12} textAlign="center">
-          <Typography fontSize="30px" fontWeight="bold">
-            List closed! Stay tuned for 12.09
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={5}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Stack direction="column" justifyContent="center" alignItems="center">
-            <Typography fontSize="25px">Join our discord</Typography>
-            <Link href="https://discord.com/invite/YpC7qw22Pb">
-              <Image
-                src={DiscordLogo}
-                alt="discordLogo"
-                height={300}
-                width={300}
-              />
-            </Link>
-          </Stack>
-        </Grid>
-        <Grid item xs={12} sm={12} md={7}>
-          <iframe
-            src="https://mint.webmint.io/mint-module/cgc-membership"
-            width="980"
-            height="650"
-            frameBorder="0"
-            style={{
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              border: "3px solid transparent",
-              borderRadius: "20px",
-            }}
-          ></iframe>
-        </Grid>
-      </Grid>
+      <Link href="https://discord.com/invite/YpC7qw22Pb" target="blank">
+        <Image
+          src={DiscordLogo}
+          alt="discord logo"
+          height={500}
+          width={500}
+          style={{ height: "100%", width: "100%" }}
+        />
+      </Link>
     </Stack>
   );
 }
