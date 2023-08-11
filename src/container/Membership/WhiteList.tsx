5b85bbaecf92c2f2e755b40a050e73a25ff11d3f
Maria del Mar <chocoinformatics2016@gmail.com>
2023-08-11 13:25:25 +0200

    added module, deleted whitelistform, discordlink

--- src/container/Membership/WhiteList.tsx
+++ src/container/Membership/WhiteList.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import WhiteListForm from "@/src/Components/WhiteListForm/WhiteListForm";
-import { Box, Grid, Stack } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+import Image from "next/image";
+import DiscordLogo from "../../../public/Images/DiscordLogo/concours-discord-cartes-voeux-fortnite-france-6.png";
 
 function WhiteList() {
   return (
@@ -12,17 +15,51 @@ function WhiteList() {
       justifyContent="center"
     >
       <div id="joinWhitelist">
-        <h2>Join the Whitelist</h2>
+        <h2>Join us</h2>
       </div>
+
       <Grid container spacing={4}>
-        <Grid item xs={12} md={4} alignContent="center">
-          <p>
-            CGC NFTs are dropping soon and you’re one of the first to know. Fill
-            out the following form to join the exclusive whitelist.
-          </p>
+        <Grid item xs={12} sm={12} md={12} textAlign="center">
+          <Typography fontSize="30px" fontWeight="bold">
+            List closed! Stay tuned for 12.09
+          </Typography>
         </Grid>
-        <Grid item xs={12} md={8}>
-          <WhiteListForm formStyles={{ width: "100%" }} />
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={5}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Stack direction="column" justifyContent="center" alignItems="center">
+            <Typography fontSize="25px">Join our discord</Typography>
+            <Link href="https://discord.com/invite/YpC7qw22Pb">
+              <Image
+                src={DiscordLogo}
+                alt="discordLogo"
+                height={300}
+                width={300}
+              />
+            </Link>
+          </Stack>
+        </Grid>
+        <Grid item xs={12} sm={12} md={7}>
+          <iframe
+            src="https://mint.webmint.io/mint-module/cgc-membership"
+            width="980"
+            height="650"
+            frameBorder="0"
+            style={{
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              border: "3px solid transparent",
+              borderRadius: "20px",
+            }}
+          ></iframe>
         </Grid>
       </Grid>
     </Stack>
