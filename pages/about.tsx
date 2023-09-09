d68069e65022d5a0ed9d5c6fb73a4567a84ae2e7
Maria del Mar <chocoinformatics2016@gmail.com>
2023-09-09 12:38:29 -0500

    added the google tags to all pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,10 +9,25 @@ import NewsLetterBox from "@/src/Components/NewsLetterBox/NewsLetterBox";
 import CarrouselPart from "@/src/container/About/CarrouselPart";
 import VideoPartLisk from "@/src/container/About/VideoPartLisk";
 import TeamPart from "@/src/container/About/TeamPart";
+import Script from "next/script";
 
 function about() {
   return (
     <Layout>
+      {/* <!-- Google tag (gtag.js) --> */}
+
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-BLGEL8RXRV"
+      />
+      <Script id="google tag">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BLGEL8RXRV');`}
+      </Script>
       <Stack overflow="hidden">
         <Header />
         <TeamPart />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,24 @@ import Section3 from "@/src/container/Home/Section3";
 import Layout from "@/src/Components/Layout/Layout";
 import React from "react";
 import NewsLetterBox from "@/src/Components/NewsLetterBox/NewsLetterBox";
+import Script from "next/script";
 
 function Home() {
   return (
     <>
       <Layout>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BLGEL8RXRV"
+        />
+        <Script id="google tag">
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BLGEL8RXRV');`}
+        </Script>
         <Section1 />
         <Section2 />
         <Section3 />
