d4c85c5c9f366efdf28f961a5eaf66402f25076d
CryptoGirlsClub <126461536+CryptoGirlsClub@users.noreply.github.com>
2024-01-30 10:28:20 -0500

    Update index.tsx

--- public/Data/MenuButtonsData/index.tsx
+++ public/Data/MenuButtonsData/index.tsx
@@ -7,7 +7,7 @@ interface MenuItem {
 const menuButtons: MenuItem[] = [
   { label: "HOME", href: "/", value: "Home" },
   { label: "ABOUT", href: "/about", value: "About" },
-  {label: "JOBS", href: "https://www.cgc.xyz/get-hired", value: "Jobs"}
+  {label: "JOBS", href: "https://www.cgc.xyz/get-hired", value: "Jobs"},
   { label: "MEMBERSHIP", href: "/join-the-club", value: "Membership" },
   { label: "CONTACT", href: "/contact", value: "Contact" },
 ];
