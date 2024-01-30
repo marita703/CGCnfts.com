bde52c3ee077337539f432a9e3bef335d8ac4762
Maria del Mar <chocoinformatics2016@gmail.com>
2024-01-30 10:02:58 -0500

    added jobs

--- public/Data/MenuButtonsData/index.tsx
+++ public/Data/MenuButtonsData/index.tsx
@@ -8,9 +8,9 @@ const menuButtons: MenuItem[] = [
   { label: "HOME", href: "/", value: "Home" },
   { label: "ABOUT", href: "/about", value: "About" },
   { label: "MEMBERSHIP", href: "/join-the-club", value: "Membership" },
+  { label: "JOBS", href: "https://www.cgc.xyz/get-hired", value: "Jobs" },
   { label: "CONTACT", href: "/contact", value: "Contact" },
   { label: "MEET THE TEAM", href: "/team", value: "Meet the team" },
-
 ];
 
 export default menuButtons;
